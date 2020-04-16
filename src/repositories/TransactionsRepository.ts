bdcccabe04cdd20aad4201d061779033c46db843
lndrmts <mateus6leandro@gmail.com>
2020-04-16 08:24:52 -0300

    Challenge completed successfully

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((accumulatorIncome, transaction) => {
+        return transaction.value + accumulatorIncome;
+      }, 0);
+
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((accumulatorOutcome, transaction) => {
+        return transaction.value + accumulatorOutcome;
+      }, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create(
+    title: string,
+    value: number,
+    type: 'income' | 'outcome',
+  ): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
